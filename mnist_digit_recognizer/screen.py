fa83db09e9fa15ee450a6c62615244bc9f9811ff
Anderson <anderson.rozaboni@sec4you.com.br>
2018-11-21 18:53:40 -0200

    Report Screen

--- mnist_digit_recognizer/screen.py
+++ mnist_digit_recognizer/screen.py
@@ -12,9 +12,13 @@ from threading import Thread
 from interface import ScreenInterface
 from util import save_prediction
 
+#Plotting
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
 import numpy as np
 
-from tkinter import Tk, Toplevel, Label, Button, Entry, filedialog, StringVar
+from tkinter import Tk, Toplevel, Label, Button, Entry, filedialog, StringVar, Frame
     
 class TFMLPScreen(NNScreenAbstract):
     
@@ -214,8 +218,127 @@ class LoadData(ScreenInterface, Toplevel):
     
         return
     
+class ReportScreen(ScreenInterface, Toplevel):
+    def __init__(self, title="Reports"):
+        Toplevel.__init__(self)
+        
+        self.create_model()
+        self.set_position()        
+        
+        self.title(title)
+        self.geometry("900x700+100+100")
+
+        
+        return
+    
+    def create_model(self):
+        
+        self.lb0 = Label(self, text="Estatísticas ")
+        self.lb1 = Label(self, text="Feedforward ")
+        self.lb2 = Label(self, text="Custo Final: ")
+        self.lb3 = Label(self, text="Taxa de Acerto: ")
+        self.lb4 = Label(self, text="Qtde de Acerto: ")
+        self.lb5 = Label(self, text="Tempo Decorrido: ")
+        self.lb6 = Label(self, text="RNN ")
+        self.lb7 = Label(self, text="Custo Final: ")
+        self.lb8 = Label(self, text="Taxa de Acerto: ")
+        self.lb9 = Label(self, text="Qtde de Acerto: ")
+        self.lb10 = Label(self, text="Tempo Decorrido: ") 
+        self.lb11 = Label(self, text="CNN ")
+        self.lb12 = Label(self, text="Custo Final: ")
+        self.lb13 = Label(self, text="Taxa de Acerto: ")
+        self.lb14 = Label(self, text="Qtde de Acerto: ")
+        self.lb15 = Label(self, text="Tempo Decorrido: ")
+        self.lb16 = Label(self, text=" ")
+        self.lb17 = Label(self, text=" ")
+        self.lb18 = Label(self, text=" ")
+        self.lb19 = Label(self, text="Gráficos ")
+        self.lb20 = Label(self, text="Gráfico Tempo x Iteração: ")
+        self.lb21 = Label(self, text="Gráfico Custo x Acerto: ")
+        self.lb22 = Label(self, text="Gráfico Custo x Iteração: ")
+        
+        self.ed1 = Entry(self,)
+        self.ed2 = Entry(self,)
+        self.ed3 = Entry(self,)
+        self.ed4 = Entry(self,)
+        self.ed5 = Entry(self,)
+        self.ed6 = Entry(self,)
+        self.ed7 = Entry(self,)
+        self.ed8 = Entry(self,)
+        self.ed9 = Entry(self,)
+        self.ed10 = Entry(self,)
+        self.ed11 = Entry(self,)
+        self.ed12 = Entry(self,)
+    
+        self.btn1 = Button(self, text="Gerar")
+        self.btn2 = Button(self, text="Gerar")
+        self.btn3 = Button(self, text="Gerar")
+        
+        #Plotting MLP
+        self.plot_frame = Frame(self, width=100, height=100, background='white')
+        self.figure = Figure()
+        self.ax = self.figure.add_subplot(111)
+        self.graph = FigureCanvasTkAgg(self.figure, master=self.plot_frame)
+        
+        return
+    
+    def set_position(self):
+        # Feedforward    
+        self.lb1.grid(row=4, column=0)
+        self.lb2.grid(row=6, column=0)
+        self.lb3.grid(row=7, column=0)
+        self.lb4.grid(row=8, column=0)
+        self.lb5.grid(row=9, column=0)
+    
+        # RNN
+        self.lb6.grid(row=4, column=2)
+        self.lb7.grid(row=6, column=2)
+        self.lb8.grid(row=7, column=2)
+        self.lb9.grid(row=8, column=2)
+        self.lb10.grid(row=9, column=2)
+     
+        # CNN
+        self.lb11.grid(row=4, column=4)
+        self.lb12.grid(row=6, column=4)
+        self.lb13.grid(row=7, column=4)
+        self.lb14.grid(row=8, column=4)
+        self.lb15.grid(row=9, column=4)
+        
+        # Label's de divisão
+        self.lb17.grid(row=12, column=0)
+        self.lb18.grid(row=15, column=0)
+        
+        self.ed1.grid(row=6, column=1)
+        self.ed2.grid(row=7, column=1)
+        self.ed3.grid(row=8, column=1)
+        self.ed4.grid(row=9, column=1)
+        self.ed5.grid(row=6, column=3)
+        self.ed6.grid(row=7, column=3)
+        self.ed7.grid(row=8, column=3)
+        self.ed8.grid(row=9, column=3)
+        self.ed9.grid(row=6, column=5)
+        self.ed10.grid(row=7, column=5)
+        self.ed11.grid(row=8, column=5)
+        self.ed12.grid(row=9, column=5)
+        
+        # Label's de divisão
+        self.lb19.grid(row=16, column=2)
+        self.lb20.grid(row=17, column=2)
+        self.btn1.grid(row=17, column=3)
+        self.lb21.grid(row=18, column=2)
+        self.btn2.grid(row=18, column=3)
+        self.lb22.grid(row=19, column=2)
+        self.btn3.grid(row=19, column=3)
+
+        #Plotting
+        self.plot_frame.grid(row=20, column=1, columnspan=8)
+        self.ax.grid()
+        self.graph.get_tk_widget().pack(side='top', fill='both', expand=True)
+        
+        return
+    
 if __name__ == "__main__":
    
-    obj = MainScreen()
+    obj = ReportScreen()
     obj.mainloop()
    
